8bcc018e0738c13a65b54f3dcb252a446a39be46
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-03 14:54:45 +0700

    Create ato.js

---
+++ ato.js
@@ -0,0 +1,10 @@
+js start
+import os
+import time
+import sys 
+Coded nanagsaparite588
+exec.import
+import end
+import start
+import marshal
+(@5585658000000000000=000000000000x000000000jfid000000000000:'=######:*)$*!:)'=,,!"/*?"*,"$!""/¤©\\\|>¥€...&=proses'*''======000000000000000000000x00000000000000000000000000050000000000)
